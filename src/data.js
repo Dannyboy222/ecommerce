b5d4c415dbc520e4c5816584ad557147f7ef6c15
Danny <ljw5040@gmail.com>
2022-02-08 18:15:04 -0800

    made a list of items on data.js  for slider

---
+++ src/data.js
@@ -0,0 +1,25 @@
+import candle1 from './Img/candle1.jpeg';
+
+export const sliderItems = [
+	{
+		id: 1,
+		img: 'https://image.s5a.com/is/image/TheBay/850017194595_main?wid=984&hei=1312&qlt=90&resMode=sharp2&op_usm=0.9,1.0,8,0',
+		title: 'WINTER SALE',
+		desc: 'Gareth sells candles',
+		bg: 'f5fafd',
+	},
+	{
+		id: 2,
+		img: 'https://image.s5a.com/is/image/TheBay/711221984115_main?wid=984&hei=1312&qlt=90&resMode=sharp2&op_usm=0.9,1.0,8,0',
+		title: 'SPRING SALE',
+		desc: 'Gareth sells candles',
+		bg: 'fcf1ed',
+	},
+	{
+		id: 3,
+		img: 'https://image.s5a.com/is/image/TheBay/810012092678_main?wid=984&hei=1312&qlt=90&resMode=sharp2&op_usm=0.9,1.0,8,0',
+		title: 'SUMMER SALE',
+		desc: 'Gareth sells candles',
+		bg: 'f5fafd',
+	},
+];
