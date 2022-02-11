1d8ad763d3c0fa6a2bac33581a1ebcf29685d646
Danny <ljw5040@gmail.com>
2022-02-10 18:25:42 -0800

    created single product page

---
+++ src/Pages/Product.jsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+import Announcement from '../components/Announcement';
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+import Newsletter from '../components/Newsletter';
+
+const Product = () => {
+	return <Container></Container>;
+};
+
+export default Product;
--- src/Pages/ProductList.jsx
+++ src/Pages/ProductList.jsx
@@ -1,6 +1,9 @@
 import styled from 'styled-components';
 import Announcement from '../components/Announcement';
 import Navbar from '../components/Navbar';
+import Products from '../components/Products';
+import Footer from '../components/Footer';
+import Newsletter from '../components/Newsletter';
 
 const Container = styled.div``;
 const Title = styled.h1`
@@ -14,6 +17,19 @@ const Filter = styled.div`
 	margin: 20px;
 `;
 
+const FilterText = styled.span`
+	font-size: 20px;
+	font-weight: 600;
+	margin-right: 20px;
+`;
+
+const Select = styled.select`
+	padding: 10px;
+	margin-right: 20px;
+`;
+
+const Option = styled.option``;
+
 const ProductList = () => {
 	return (
 		<Container>
@@ -22,9 +38,29 @@ const ProductList = () => {
 
 			<Title>Candles</Title>
 			<FilterContainer>
-				<Filter>filter1</Filter>
-				<Filter>filter2</Filter>
+				<Filter>
+					<FilterText>Filter Products:</FilterText>
+					<Select>
+						<Option disabled selected>
+							Size
+						</Option>
+						<Option>Small</Option>
+						<Option>Medium</Option>
+						<Option>Large</Option>
+					</Select>
+				</Filter>
+				<Filter>
+					<FilterText>Sort Products:</FilterText>
+					<Select>
+						<Option selected>Newest</Option>
+						<Option>Price (asc)</Option>
+						<Option>Price (desc)</Option>
+					</Select>
+				</Filter>
 			</FilterContainer>
+			<Products />
+			<Newsletter />
+			<Footer />
 		</Container>
 	);
 };
