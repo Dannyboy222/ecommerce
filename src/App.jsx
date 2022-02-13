2843f454aa28ee80aa27f5975c967475df87a9e1
Danny <ljw5040@gmail.com>
2022-02-12 16:17:19 -0800

    created register and login pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,13 @@ import './App.css';
 import Home from './Pages/Home';
 import Product from './Pages/Product';
 import ProductList from './Pages/ProductList';
+import Register from './Pages/Register';
 
 function App() {
 	return (
 		<div className="App">
 			{/* <Home /> */}
-			{/* <ProductList /> */}
-			<Product />
+			<Register />
 		</div>
 	);
 }
