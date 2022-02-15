42ec6d850b876151ef3c470158e8b09f3fbfeaf9
Danny <ljw5040@gmail.com>
2022-02-14 19:38:27 -0800

    finished cart page

--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -46,10 +46,6 @@ const Info = styled.div`
 	flex: 3;
 `;
 
-const Summary = styled.div`
-	flex: 1;
-`;
-
 const Product = styled.div`
 	display: flex;
 	justify-content: space-between;
@@ -97,6 +93,31 @@ const Hr = styled.hr`
 `;
 const ProductId = styled.span``;
 const ProductSize = styled.span``;
+const Summary = styled.div`
+	flex: 1;
+	border: 0.5px solid lightgray;
+	border-radius: 10px;
+	padding: 20px;
+	height: 50vh;
+`;
+const SummaryTitle = styled.h1`
+	font-weight: 200;
+`;
+const SummaryItem = styled.div`
+	margin: 30px 0px;
+	display: flex;
+	justify-content: space-between;
+	font-weight: ${(props) => props.type === 'total' && '500'};
+	font-size: ${(props) => props.type === 'total' && '24px'};
+`;
+const SummaryItemText = styled.span``;
+const SummaryItemPrice = styled.span``;
+const SummaryButton = styled.button`
+	width: 100%;
+	padding: 10px;
+	background-color: black;
+	color: white;
+`;
 
 const Cart = () => {
 	return (
@@ -165,7 +186,26 @@ const Cart = () => {
 							</PriceDetail>
 						</Product>
 					</Info>
-					<Summary>Summary</Summary>
+					<Summary>
+						<SummaryTitle>ORDER SUMMARY</SummaryTitle>
+						<SummaryItem>
+							<SummaryItemText>Subtotal</SummaryItemText>
+							<SummaryItemPrice>$ 40</SummaryItemPrice>
+						</SummaryItem>
+						<SummaryItem>
+							<SummaryItemText>Estimated Shipping</SummaryItemText>
+							<SummaryItemPrice>$ 6.80</SummaryItemPrice>
+						</SummaryItem>
+						<SummaryItem>
+							<SummaryItemText>Shipping Discount</SummaryItemText>
+							<SummaryItemPrice>$ -4.90</SummaryItemPrice>
+						</SummaryItem>
+						<SummaryItem type="total">
+							<SummaryItemText>Total</SummaryItemText>
+							<SummaryItemPrice>$ 41.90</SummaryItemPrice>
+						</SummaryItem>
+						<SummaryButton>CHECKOUT NOW</SummaryButton>
+					</Summary>
 				</Bottom>
 			</Wrapper>
 			<Footer />
